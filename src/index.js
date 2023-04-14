32f95a7e364c063ea03ee868d491f6f204e2b5e5
SenemDEV <121942044+SenemDEV@users.noreply.github.com>
2023-04-14 10:51:50 +0200

    add auth. component + initila set up

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+// import 'bootstrap/dist/css/bootstrap.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { AuthProviderWrapper } from "./context/auth.context";
+import { BrowserRouter as Router } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+    <AuthProviderWrapper>
+     <App />
+    </AuthProviderWrapper>
+    </Router>
   </React.StrictMode>
 );
 
