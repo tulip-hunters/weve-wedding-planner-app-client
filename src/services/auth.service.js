258995e31d6fd7ee7965ccad31cac73a1836cefd
SenemDEV <121942044+SenemDEV@users.noreply.github.com>
2023-04-18 13:01:45 +0200

    change environment variable to server

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -4,7 +4,7 @@ class AuthService {
   constructor() {
     // Create a new instance of axios with a custom configuration
     this.api = axios.create({
-      baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5005'
+      baseURL: process.env.REACT_APP_SERVER_URL || 'http://localhost:5005'
       // We set our API's base URL so that all requests use the same base URL
     });
 
