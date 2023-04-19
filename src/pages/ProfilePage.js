42c2b5012aea2b2339c37002521960ac4fa9c1c4
SenemDEV <121942044+SenemDEV@users.noreply.github.com>
2023-04-19 18:47:07 +0200

    add profile page + css

--- src/pages/ProfilePage.js
+++ src/pages/ProfilePage.js
@@ -1,26 +1,69 @@
-import React, { useContext } from "react";
+import Image from "react-bootstrap/Image";
+import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../context/auth.context";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import "./ProfilePage.css"
 
 const ProfilePage = () => {
   const { user } = useContext(AuthContext);
+  const [venues, setVenues] = useState({});
+
+  useEffect(() => {
+    // Fetch all venues and store them in state
+    axios
+      .get(`${process.env.REACT_APP_SERVER_URL}/api/venues`)
+      .then((response) => {
+        const allVenues = response.data.reduce((acc, venue) => {
+          acc[venue._id] = venue.name;
+          // acc[venue.imageUrl] = venue.imageUrl;
+          return acc;
+        }, {});
+        setVenues(allVenues);
+      })
+      .catch((error) => console.log(error));
+  }, []);
 
   return (
     <div>
       {user ? (
         <>
-          <p>{user.name}</p>
-          <p>Your reservations</p>
+          <p className="user">
+            {" "}
+            {user.name},
+            <br /> your email address assosiated with our website is: <p className="email">{user.email}</p>
+          </p>{" "}
+          <br />
+          <br />
+          <p className="reservation-title" style={{background:"purple", color:"white"}}>CURRENTLY YOU HAVE THE FOLLOWING RESERVATIONS:</p>
           {user.reservations ? (
             user.reservations.map((reservation) => {
-              return <p>{reservation.title}</p>;
+              const venueName = venues[reservation.venue];
+              return (
+                <div className="card" key={reservation._id}>
+                  <p className="reservation" style={{background:"purple", color:"white"}}>{reservation.title}</p>
+                  <p className="reservation-date">
+                    {new Date(reservation.weddingDate).toLocaleString("en-uk", {
+                      weekday: "long",
+                      day: "numeric",
+                      month: "long",
+                      year: "numeric",
+                    })}
+                  </p>
+                  {venueName && <p className="venue-name">{venueName}</p>}
+                </div>
+              );
             })
           ) : (
-            <p>no reservation</p>
+            <p className="reservation-title" style={{background:"purple", color:"white"}} >Currently there are no reservations to display</p>
           )}
         </>
       ) : (
-        <p>no user</p>
+        <p>Please sign in</p>
       )}
+      <Link to="/venues">
+        <button className="btn btn-purple text-white" >Check other options</button>
+      </Link>
     </div>
   );
 };
